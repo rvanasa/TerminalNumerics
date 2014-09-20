a41bf82d4a3925442e16bdb7a5309540f682ed57
rvanasa <ryanvandersmith@hotmail.com>
2014-09-20 09:23:01 -0600

    Made test class platform-independent

--- src/test/java/net/anasa/math/test/Test.java
+++ src/test/java/net/anasa/math/test/Test.java
@@ -1,30 +1,12 @@
 package net.anasa.math.test;
 
-import javax.swing.ImageIcon;
-
-import net.anasa.util.Progress;
-import net.anasa.util.ui.SplashScreenComponent;
+import net.anasa.math.ui.GraphInputComponent;
 import net.anasa.util.ui.WindowComponent;
 
 public class Test
 {
 	public static void main(String... argv) throws Exception
 	{
-		WindowComponent window = new WindowComponent("Test Display", new SplashScreenComponent(new ImageIcon("E:/Pictures/Anasa/AnasaRenderVingette_Thumbnail.jpg"), Progress.start("test", 10), () -> {
-			for(int i = 0; i < 10; i++)
-			{
-				try
-				{
-					Thread.sleep(100);
-					Progress.increment("test");
-				}
-				catch(Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}));
-		window.setFrameVisible(false);
-		window.display();
+		new WindowComponent("Graph Interface", new GraphInputComponent()).display();
 	}
 }
