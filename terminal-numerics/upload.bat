e95bd45b3d25b8d921ce36406ad2c87fb98cf88e
rvanasa <ryanvandersmith@hotmail.com>
2014-11-30 12:49:03 -0700

    Added platform-dependent upload script

---
+++ terminal-numerics/upload.bat
@@ -0,0 +1,4 @@
+cd target
+rename export terminal-numerics
+"C:\Program Files (x86)\7-Zip\7z.exe" a -tzip terminal-numerics.zip terminal-numerics
+move terminal-numerics.zip "\\RV344100_SERVER\Users\Server\Documents\Tomcat\webapps\ROOT\terminal-numerics.zip"
