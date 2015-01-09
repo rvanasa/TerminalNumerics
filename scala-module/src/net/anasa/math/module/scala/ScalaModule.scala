405cfd7ebcb8e880f8048e1851fef01d907cfcac
rvanasa <ryanvandersmith@hotmail.com>
2015-01-08 18:55:56 -0700

    Renamed 'ScalaModule' to 'scala-module'

--- scala-module/src/net/anasa/math/module/scala/ScalaModule.scala
+++ scala-module/src/net/anasa/math/module/scala/ScalaModule.scala
@@ -2,9 +2,10 @@ package net.anasa.math.module.scala;
 
 import rchs.tsa.math.resource.module.IModuleDelegate
 
-object ScalaModule extends IModuleDelegate {
-	@Override
-	def init() {
+object ScalaModule extends IModuleDelegate
+{
+	override def init()
+	{
 		System.out.println("Scala module loaded!");
 	}
 }
